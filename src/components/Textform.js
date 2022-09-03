bff0f98fac88e145a9ba9564858e029fc8959978
Afaq Hussain <hussainafaq18@gmail.com>
2022-09-04 03:09:33 +0500

    Update new click functions in text form

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -4,28 +4,64 @@ import PropTypes from 'prop-types'
 export default function Textform(props) {
     
     const handleUpClick =()=> {
-        console.log("Upper case was clicked" + text)
+ 
         let text2 = text.toUpperCase();
         setText(text2);
+    }
+    
+    const handleLowClick =()=> {
+        let text3 = text.toLowerCase();
+        setText(text3);
+    }
+
+    const handleDoubleClick =()=> {
+ 
+        let text4 = text.repeat(2);
+        setText(text4);
+    }
+    
+    const handleClearClick =()=> {
+ 
+        let text5 =' ';
+        setText(text5);
     }
 
     const handleOnChange = (event) => {
         console.log("On change");
         setText(event.target.value);
     }
- 
-
 
     const [text, setText] = useState('Enter text here');
-    //setText("Enter here!!!");
+
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
     <div className="mb-3">
         <textarea className="form-control" value = {text} onChange = {handleOnChange} id="myTextBox" rows="5"></textarea>
     </div>
-       <button className="btn btn-primary" onClick={handleUpClick}> Click to convert text to Upper case</button>
+       <button className="btn btn-primary mx-3" 
+       onClick={handleUpClick}> Click to convert text to Upper case</button>
+
+       <button className="btn btn-primary mx-3" 
+       onClick={handleLowClick}> Click to convert text to Lower Case</button>
+    
+        <button className="btn btn-primary mx-3" 
+       onClick={handleDoubleClick}> Click to repeat the text twice in the box</button>
+
+       <button className="btn btn-primary mx-3" 
+       onClick={handleClearClick}> Click to clear the screen</button>
     </div>
+
+    <div className="container">
+        <h2>Your text Summary</h2>
+        <p>{text.split(" ").length} word and {text.length} characters</p>
+        <p>You need {0.008 * text.split(" ").length} Minutes to read this</p>
+        <br/>
+        <h3>Preview</h3>
+        <p>{text}</p>
+    </div>
+    </>
   )
 }
 
